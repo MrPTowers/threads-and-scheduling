9a1d5ebb462ecc13cd6611f569811369bd2bbedc
MrPTowers <pablourayoan1@gmail.com>
2025-09-20 23:49:17 -0400

    adding initial cluster code

--- cluster.py
+++ cluster.py
@@ -1,3 +1,38 @@
+import logging
 import threading
 import time
-import os
+
+
+def consumer(cond):
+    """wait for the condition and use the resource"""
+    logging.debug('Starting consumer thread')
+    with cond:
+        cond.wait()
+        logging.debug('Resource is available to consumer')
+
+
+def producer(cond):
+    """set up the resource to be used by the consumer"""
+    logging.debug('Starting producer thread')
+    with cond:
+        logging.debug('Making resource available')
+        cond.notifyAll()
+
+
+threads = []
+condition = threading.Condition()
+c1 = threading.Thread(name='c1', target=consumer,
+                      args=(condition,))
+c2 = threading.Thread(name='c2', target=consumer,
+                      args=(condition,))
+p = threading.Thread(name='p', target=producer,
+                     args=(condition,))
+threads.append(c1)
+threads.append(c2)
+threads.append(p)
+
+for t in threads:
+	t.start()
+
+for t in threads:
+	t.join()
