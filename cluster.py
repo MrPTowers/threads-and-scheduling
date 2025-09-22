6882a54178bade06b83eddc950cb29f6783a5d63
MrPTowers <pablourayoan@gmail.com>
2025-09-22 14:50:43 -0400

    finished implementation of cluster and edevice

--- cluster.py
+++ cluster.py
@@ -3,53 +3,81 @@ import time
 import socket
 import sys
 
-#host = '127.0.0.1'
-#port = sys.argv[1]
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = '127.0.0.1'
+port = int(sys.argv[1])
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+queue_max = 10
 queue = []
-lock = threading.lock()
-semaphore = threading.Semaphore(value=2)
+lock = threading.Lock()
+empty_slots = threading.Semaphore(queue_max)
+full_slots = threading.Semaphore(0)
 
+proc_tables = {}
+producer_done = threading.Event()
 
-
-def consumer():
-	proc_table = []
-	while True:
-		lock.acquire() ##Critical region start
-		process = queue[0]
-		queue.pop(1)
-		lock.release() ##Critical region end
-		proc_table.append(process)
-		time.sleep(process[1])
-
-##consumer END
-
-def producer(): 
+def consumer(thread_id):
+    proc_table = []
     while True:
+        lock.acquire() #Critical region start
+        if not queue and producer_done.is_set():
+            lock.release()
+            break
+        lock.release()
+        full_slots.acquire()
         
-        ##Listen for message
-        ##Store the tuple in queue
-        queue.sort(key=shortestJobFirst())
+        lock.acquire()
+        if queue:
+            process = queue.pop(0)
+            print(f"Consumer {thread_id} ran {process[0]}")
+        else:
+            lock.release()
+            full_slots.release()
+            continue
+        lock.release() #Critical region end
+        
+        empty_slots.release()
+        proc_table.append(process)
+        time.sleep(process[1])
+    proc_tables[thread_id] = proc_table
 
-##producer END
+#consumer END
+
+def producer(clientSocket):    
+    while True:
+        msg = clientSocket.recv(1024).decode()
+        if not msg or msg == 'done':
+            break
+        parts = msg.split(',')
+        job = [parts[0], int(parts[1])]
+        empty_slots.acquire()
+        lock.acquire() #Critical region start
+        queue.append(job)
+        queue.sort(key=shortestJobFirst)
+        lock.release() #Critical region end
+        full_slots.release()
+        time.sleep(0.5)
+    producer_done.set()
+    print("Producer finished sending jobs.")
+
+#producer END
 
 def shortestJobFirst(e):
-    return e[1] ##Return second element of the tuple (execution time)
+    return e[1] #Return second element of the tuple (execution time)
 
 
 def main():
-    #s.bind(host,port)
-    #s.listen(5)
+    s.bind((host,port))
+    s.listen() 
+    print(f"Server listening on  {host}:{port}")
 
-        #while True
-            #clientSocket, address = s.accept()
-            #print(f"Connection estavlished from address {address}")    
+    clientSocket, address = s.accept()
+    print(f"Connection established from address {address}") 
 
     threads = []
 
-    c1 = threading.Thread(name='c1', target=consumer, args=(i,))
-    c2 = threading.Thread(name='c2', target=consumer, args=(i,))
-    p = threading.Thread(name='p', target=producer, args=(i,))
+    c1 = threading.Thread(name='c1', target=consumer, args=(1,))
+    c2 = threading.Thread(name='c2', target=consumer, args=(2,))
+    p = threading.Thread(name='p', target=producer, args=(clientSocket,))
 
     threads.append(c1)
     threads.append(c2)
@@ -60,19 +88,27 @@ def main():
 
     for t in threads:
         t.join()
-    #else:
-     #   sys.exit(1)
-      #  socket.close()
 
-##main END
+    results = ""
+    for cid, table in proc_tables.items():
+        results+= f"Consumer {cid} processed:\n"
+        for job in table:
+            results += f"{job[0]},{job[1]}\n"
+        results += "---\n"
+
+    clientSocket.sendall(results.encode())
+    clientSocket.close()
+    s.close()
+
+#main END
 
 
 if __name__ == "__main__":
-    ##Check to see if program is run with the correct amount of arguments
+    #Check to see if program is run with the correct amount of arguments
     if len(sys.argv) == 2:
-    	main(port)
+    	main()
     else:
-    	##Disclose correct usage of program
+    	#Disclose correct usage of program
         print("Usage: python3 cluster.py <port_number>")
         sys.exit(1)
 
--- edevice.py
+++ edevice.py
@@ -1,32 +1,42 @@
 import socket
 import sys
-import time
 import subprocess
 import random
+import time
 
 host = sys.argv[1]
-port = sys.argv[2]
-s = socket.socket(socket.AF_INTER, socket.SOCK_STREAM)
+port = int(sys.argv[2])
 
-##Missing the connection to server with big number port implementation
+def main(): 
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    
+    process_list = subprocess.run(['ps', '-eo', 'comm='], capture_output=True, text=True).stdout.splitlines()
 
-def main:
-   
+    for line in process_list:
+        msg = f"{line.strip()},{random.randint(1,5)}"
+        print(msg)
+        s.send(msg.encode())
+        time.sleep(1)
 
+    time.sleep(5)
+    s.send("done".encode())
+    while True:
+        data = s.recv(1024)
+        if not data:
+            continue
+        print(f"\nMessage received:\n {data.decode()}")
+        break
 
-    process_list = subprocess.run(['ps', '-eo', 'comm='], capture_output=True, text=True).stdout
-        
-    for line in process:
-        message = [line.strip(),random.randint(1,5)]
-        s.send(message..encode())
+    s.close()
 
 ##main END
 
-if __name__ == '__main__'
-    ##Check to see if program is run with the correct amount of arguments
+if __name__ == '__main__':
+    #Check to see if program is run with the correct amount of arguments
     if len(sys.argv) == 3:
         main()
     else:
-    ##Disclose correct usage of program
+    #Disclose correct usage of program
         print("Usage: python3 edevice.py <server_address> <port_number>")
         sys.exit(1)
